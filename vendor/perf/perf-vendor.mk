c225215388690186cd1af4e2b2829159776abb53
Akash Srivastava <akashniki@gmail.com>
2026-08-14 15:48:37 +0200

    common: perf: update for sun platform
    Change-Id: I5e8f4448c97e6ce4418e6d34b44573dfa964fd5f

--- vendor/perf/perf-vendor.mk
+++ vendor/perf/perf-vendor.mk
@@ -11,7 +11,9 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/common/vendor/perf/proprietary/vendor/bin/hw/vendor.qti.hardware.servicetrackeraidl-service:$(TARGET_COPY_OUT_VENDOR)/bin/hw/vendor.qti.hardware.servicetrackeraidl-service \
     vendor/qcom/common/vendor/perf/proprietary/vendor/bin/msm_irqbalance:$(TARGET_COPY_OUT_VENDOR)/bin/msm_irqbalance \
     vendor/qcom/common/vendor/perf/proprietary/vendor/bin/poweropt-service:$(TARGET_COPY_OUT_VENDOR)/bin/poweropt-service \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/bin/snapdragon_services:$(TARGET_COPY_OUT_VENDOR)/bin/snapdragon_services \
     vendor/qcom/common/vendor/perf/proprietary/vendor/etc/init/poweropt-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/poweropt-service.rc \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/etc/init/snapdragon_services.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/snapdragon_services.rc \
     vendor/qcom/common/vendor/perf/proprietary/vendor/etc/init/vendor.qti.hardware.iop@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.iop@2.0-service.rc \
     vendor/qcom/common/vendor/perf/proprietary/vendor/etc/init/vendor.qti.hardware.perf2-hal-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.perf2-hal-service.rc \
     vendor/qcom/common/vendor/perf/proprietary/vendor/etc/init/vendor.qti.hardware.servicetrackeraidl-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.qti.hardware.servicetrackeraidl-service.rc \
@@ -20,6 +22,7 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libaodoptfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libaodoptfeature.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libapengine.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libapengine.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libappclassifier.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libappclassifier.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libcamerapoweroptfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcamerapoweroptfeature.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libgame_enhance.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgame_enhance.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libgamepoweroptfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgamepoweroptfeature.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/liblearningmodule.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblearningmodule.so \
@@ -31,6 +34,7 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libmeters.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmeters.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/liboffscreenpoweroptfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liboffscreenpoweroptfeature.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libperfconfig.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libperfconfig.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libperfdb.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libperfdb.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libperfgluelayer.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libperfgluelayer.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libperfioctl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libperfioctl.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libpowercallback.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libpowercallback.so \
@@ -40,18 +44,28 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libprocomp.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprocomp.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libpsmoptfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libpsmoptfeature.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libq-perflog.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libq-perflog.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqape_oem_ext.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqape_oem_ext.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqapesdk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqapesdk.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqspm-mem-utils-vendor.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqspm-mem-utils-vendor.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-appclassifier.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-appclassifier.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-iopd-client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-iopd-client.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-iopd.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-iopd.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-jankrescue.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-jankrescue.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-lllite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-lllite.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-perfd-client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-perfd-client.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-perfd.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-perfd.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-perfextn-intf.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-perfextn-intf.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-qesdk-secure.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-qesdk-secure.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-s3tf.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-s3tf.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-scenariod.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-scenariod.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-sslite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-sslite.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-util.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-util.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libqti-utils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libqti-utils.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libsagt.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsagt.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libsilkyscrolls.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsilkyscrolls.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libskewknob.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libskewknob.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libsplh.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsplh.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libsqlite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsqlite.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libstandbyfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstandbyfeature.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/libvideooptfeature.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libvideooptfeature.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.hardware.iop@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.iop@1.0.so \
@@ -63,9 +77,12 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.hardware.perf@2.3.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.perf@2.3.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.hardware.power.powermodule-V1-ndk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.hardware.power.powermodule-V1-ndk.so \
     vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.memory.pasrmanager-V1-ndk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.memory.pasrmanager-V1-ndk.so \
-    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.qspmhal-V1-ndk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.qspmhal-V1-ndk.so
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.qspmhal-V1-ndk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.qspmhal-V1-ndk.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.snapdragonServices-V1-ndk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.snapdragonServices-V1-ndk.so \
+    vendor/qcom/common/vendor/perf/proprietary/vendor/lib64/vendor.qti.snapdragonServices.qape-V1-ndk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/vendor.qti.snapdragonServices.qape-V1-ndk.so
 
 PRODUCT_PACKAGES += \
+    snapdragon_services.xml \
     vendor.qti.hardware.perf2.xml \
     vendor.qti.hardware.power.powermodule.xml \
     vendor.qti.hardware.servicetrackeraidl-service.xml
